2b451050333ad812fa9e1811959ef8f7519d3241
Drew Noakes <git@drewnoakes.com>
2023-12-07 22:28:36 +1100

    Update

--- Client/Client.cs
+++ Client/Client.cs
@@ -18,6 +18,103 @@ async Task WatchResourcesAsync(string address, CancellationToken cancellationTok
 {
     var (channel, client) = CreateChannel(address);
 
+    var errorCount = 0;
+
+    try
+    {
+        await channel.ConnectAsync(cancellationToken);
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            if (channel.State == ConnectivityState.Shutdown)
+            {
+                Console.WriteLine("Channel has shut down. Recreating connection.");
+
+                channel.Dispose();
+
+                (channel, client) = CreateChannel(address);
+            }
+
+            if (errorCount > 0)
+            {
+                // Exponential backoff (2^(n-1)) up to a maximum.
+                TimeSpan delay = TimeSpan.FromSeconds(Math.Min(Math.Pow(2, errorCount - 1), 15));
+
+                await Task.Delay(delay, cancellationToken);
+            }
+
+            try
+            {
+                await ProcessData();
+            }
+            catch (RpcException ex)
+            {
+                errorCount++;
+                Console.WriteLine($"Error {errorCount} watching resources: {ex.Message}");
+            }
+        }
+    }
+    finally
+    {
+        channel.Dispose();
+    }
+
+
+    Console.WriteLine("Stopping resource watch");
+
+    channel.Dispose();
+
+    async Task ProcessData()
+    {
+        Console.WriteLine("Starting watch");
+
+        var call = client.WatchResources(new WatchResourcesRequest { IsReconnect = false }, cancellationToken: cancellationToken);
+
+        await foreach (var response in call.ResponseStream.ReadAllAsync(cancellationToken: cancellationToken))
+        {
+            Console.WriteLine($"Response type: {response.KindCase}");
+
+            // The most reliable way to check that a streaming call succeeded is to successfully read a response.
+            if (errorCount > 0)
+            {
+                resourceById.Clear();
+                errorCount = 0;
+            }
+
+            if (response.KindCase == WatchResourcesUpdate.KindOneofCase.InitialSnapshot)
+            {
+                // Copy initial snapshot into model.
+                foreach (var resource in response.InitialSnapshot.Resources)
+                {
+                    resourceById[resource.ResourceId] = resource;
+                }
+            }
+            else if (response.KindCase == WatchResourcesUpdate.KindOneofCase.Changes)
+            {
+                // Apply changes to the model.
+                foreach (var change in response.Changes.Value)
+                {
+                    if (change.KindCase == WatchResourcesChange.KindOneofCase.Upsert)
+                    {
+                        // Upsert (i.e. add or replace)
+                        resourceById[change.Upsert.ResourceId] = change.Upsert;
+                    }
+                    else if (change.KindCase == WatchResourcesChange.KindOneofCase.Delete)
+                    {
+                        // Remove
+                        resourceById.Remove(change.Delete.ResourceId);
+                    }
+                }
+            }
+            else
+            {
+                throw new FormatException("Unsupported response kind: " + response.KindCase);
+            }
+
+            Console.WriteLine($"Current resource count: {resourceById.Count}");
+        }
+    }
+
     static (GrpcChannel, DashboardService.DashboardServiceClient) CreateChannel(string address)
     {
         var httpHandler = new SocketsHttpHandler
@@ -37,88 +134,4 @@ async Task WatchResourcesAsync(string address, CancellationToken cancellationTok
 
         return (channel, client);
     }
-
-    var errorCount = 0;
-
-    await channel.ConnectAsync(cancellationToken);
-
-    while (!cancellationToken.IsCancellationRequested)
-    {
-        if (channel.State == ConnectivityState.Shutdown)
-        {
-            Console.WriteLine("Channel has shut down. Recreating connection.");
-
-            channel.Dispose();
-
-            (channel, client) = CreateChannel(address);
-        }
-
-        if (errorCount > 0)
-        {
-            // Exponential backoff (2^(n-1)) up to a maximum.
-            TimeSpan delay = TimeSpan.FromSeconds(Math.Min(Math.Pow(2, errorCount - 1), 15));
-
-            await Task.Delay(delay, cancellationToken);
-        }
-
-        try
-        {
-            Console.WriteLine("Starting watch");
-
-            var call = client.WatchResources(new WatchResourcesRequest { IsReconnect = false }, cancellationToken: cancellationToken);
-
-            await foreach (var response in call.ResponseStream.ReadAllAsync(cancellationToken: cancellationToken))
-            {
-                Console.WriteLine($"Response type: {response.KindCase}");
-
-                // The most reliable way to check that a streaming call succeeded is to successfully read a response.
-                if (errorCount > 0)
-                {
-                    resourceById.Clear();
-                    errorCount = 0;
-                }
-
-                if (response.KindCase == WatchResourcesUpdate.KindOneofCase.InitialSnapshot)
-                {
-                    // Copy initial snapshot into model.
-                    foreach (var resource in response.InitialSnapshot.Resources)
-                    {
-                        resourceById[resource.ResourceId] = resource;
-                    }
-                }
-                else if (response.KindCase == WatchResourcesUpdate.KindOneofCase.Changes)
-                {
-                    // Apply changes to the model.
-                    foreach (var change in response.Changes.Value)
-                    {
-                        if (change.KindCase == WatchResourcesChange.KindOneofCase.Upsert)
-                        {
-                            // Upsert (i.e. add or replace)
-                            resourceById[change.Upsert.ResourceId] = change.Upsert;
-                        }
-                        else if (change.KindCase == WatchResourcesChange.KindOneofCase.Delete)
-                        {
-                            // Remove
-                            resourceById.Remove(change.Delete.ResourceId);
-                        }
-                    }
-                }
-                else
-                {
-                    throw new FormatException("Unsupported response kind: " + response.KindCase);
-                }
-
-                Console.WriteLine($"Current resource count: {resourceById.Count}");
-            }
-        }
-        catch (RpcException ex)
-        {
-            errorCount++;
-            Console.WriteLine($"Error {errorCount} watching resources: {ex.Message}");
-        }
-    }
-
-    Console.WriteLine("Stopping resource watch");
-
-    channel.Dispose();
 }
